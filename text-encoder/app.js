f70fd8890c847dfeb926dc2395f9c744b722946e
Manas Tripathi <manastripathi25@gmail.com>
2022-04-17 03:23:59 +0530

    implemented a text-encoding webapp

---
+++ text-encoder/app.js
@@ -0,0 +1,25 @@
+const { hash } = window.location;
+const message = atob(hash.replace('#',''));
+
+if(message){
+    document.querySelector('#msg-form').classList.add('hide');
+    document.querySelector('#msg-show').classList.remove('hide');
+    document.querySelector('h1').innerHTML =message;
+}
+
+
+document.querySelector('form').addEventListener('submit', event => {
+    event.preventDefault();
+
+    document.querySelector('#msg-form').classList.add('hide');
+    document.querySelector('#link-form').classList.remove('hide');
+
+    const input = document.querySelector('#msg-input');
+    const encrypted = btoa(input.value);
+
+    const linkInput = document.querySelector('#link-input');
+    linkInput.value = `${window.location}#${encrypted}`;
+    linkInput.select(); 
+    
+    
+});
