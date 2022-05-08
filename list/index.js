ef3305a81e9d462fce2249b256963d2cce1708e1
Manas Tripathi <manastripathi25@gmail.com>
2022-05-09 01:38:06 +0530

    implemented using promises

--- list/index.js
+++ list/index.js
@@ -6,30 +6,16 @@ fs.readdir( process.cwd(), (err, filenames) => {
         //return;
         throw new Error(err);
     }
-    console.log(filenames);
-
-    const allStats = Array(filenames.length).fill(null);
-
-    for(let filename of filenames)
-    {
-        fs.lstat(filename, (err, stats) => {
-            if(err){
-                throw new Error(err);
-            }
-
-            //console.log(filename, stats.isFile());
-            allStats[index] = stats;
-
-            const ready = allStats.every((stats) => {
-                return stats;
-            });
-
-            if (ready){
-                allStats.forEach((stats,index)=>{
-                    console.log(filenames[index], stats.isFile());
-                });
-            }
-        });
-    }
-
 });
+
+const lstat = (filename) => {
+    return new Promise((resolve,reject) => {
+        fs.lstat(filename, (err,stats) => {
+            if (err) {
+                reject (err);
+            }
+
+            resolve(stats);
+        });
+    });
+}
