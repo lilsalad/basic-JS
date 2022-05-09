37c0e66c1e1fddd63a4d988a07ef77821e66d295
Manas Tripathi <manastripathi25@gmail.com>
2022-05-09 19:56:50 +0530

    parallel implementation added

--- list/index.js
+++ list/index.js
@@ -1,21 +1,45 @@
 const fs = require('fs')
+const util = require('util');
+const targetDir = process.argv[2] || process.cwd();
+/*SEQUENTIAL PROMISES
 
+const lstat = util.promisify(fs.lstat);
 fs.readdir( process.cwd(), (err, filenames) => {
     if (err) {
         //console.log(err);
         //return;
         throw new Error(err);
     }
+    for(let filename of filenames){
+        try{
+            const stats = await lstat(filename);
+            console.log(filename, stats.isFile());
+        }
+        catch (err){
+            console.log(err);
+        }
+    }
 });
+*/
 
-const lstat = (filename) => {
-    return new Promise((resolve,reject) => {
-        fs.lstat(filename, (err,stats) => {
-            if (err) {
-                reject (err);
-            }
 
-            resolve(stats);
-        });
+//PARALLEL PROMISES
+const {lstat} = fs.promises;
+
+fs.readdir( targetDir, async(err, filenames) => {
+    if (err) {
+        //console.log(err);
+        //return;
+        throw new Error(err);
+    }
+    const statPromises = filenames.map(filename => {
+        return lstat(path.join(targetDir, filename));
     });
-}
+
+    const allStats = await Promise.all(statPromises);
+
+    for(let stats of allStats){
+        const index = allStats.indexOf(stats);
+        console.log(filenames[index]);
+    }
+});
